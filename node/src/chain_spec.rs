b6c052b4573dc32d5348ca53f6e949f5b04f9e33
tungla <tungleanh.ptit@gmail.com>
2022-04-10 09:07:35 +0700

    run_private_network

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -7,6 +7,8 @@ use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{sr25519, Pair, Public};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{IdentifyAccount, Verify};
+use hex_literal::hex;
+use sp_core::crypto::UncheckedInto;
 
 // The URL for the telemetry server.
 // const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -122,6 +124,93 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 		// Extensions
 		None,
 	))
+}
+
+pub fn kitty_testnet_config() -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+
+	let initial_authorities: Vec<(AuraId, GrandpaId)> = vec![
+		(
+			hex!("6696875491867fa93795b30eb3712ae9603c97c338d25d5fe8d431165042d523").unchecked_into(),
+			hex!("e917586fededcb5494725d82078b232efde7bcd84ee5bb3817dde5892c521bbd").unchecked_into()
+		),
+		(
+			hex!("6a66d1126db9615abdbc2637b54e9455fde463a68b658f598200937eda4d0060").unchecked_into(),
+			hex!("80aa8f3cc71afa204da7721ef33b49cf727ff51acf4bbad79f30c0e907b74971").unchecked_into()
+		),
+		(
+			hex!("62161bb94215a21a428b51961a529edea594897b29763348ea5f69c29a795f4c").unchecked_into(),
+			hex!("4859c6919279ad4eb38c705e449346eea72c84e1df4467484bc38f97c5ee16f8").unchecked_into()
+		),
+	];
+	Ok(ChainSpec::from_genesis(
+		// Name
+		"Kitty Testnet",
+		// ID
+		"kit",
+		ChainType::Live,
+		move || {
+			kitty_testnet_genesis(
+				wasm_binary,
+				// Initial PoA authorities
+				initial_authorities.clone(),
+				// Sudo account
+				hex!("5cdc3f6b3f4c2d70a5cc83bdb1840da5d60aeb1191c874eed7423259c8b5676a").into(),
+				// Pre-funded accounts
+				vec![
+					hex!("5cdc3f6b3f4c2d70a5cc83bdb1840da5d60aeb1191c874eed7423259c8b5676a").into(),
+				],
+				true,
+			)
+		},
+		// Bootnodes
+		vec![],
+		// Telemetry
+		None,
+		// Protocol ID
+		None,
+		// Properties
+		None,
+		Some(
+			serde_json::from_str(
+				"{\"tokenDecimals\": 18, \"tokenSymbol\": \"KIT\", \"SS58Prefix\": 48}",
+			)
+			.expect("Provided valid json map"),
+		),
+		// Extensions
+		None,
+	))
+}
+
+/// Configure initial storage state for FRAME modules.
+fn kitty_testnet_genesis(
+	wasm_binary: &[u8],
+	initial_authorities: Vec<(AuraId, GrandpaId)>,
+	root_key: AccountId,
+	endowed_accounts: Vec<AccountId>,
+	_enable_println: bool,
+) -> GenesisConfig {
+	GenesisConfig {
+		system: SystemConfig {
+			// Add Wasm runtime to storage.
+			code: wasm_binary.to_vec(),
+		},
+		balances: BalancesConfig {
+			// Configure endowed accounts with initial balance of 1 << 60.
+			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
+		},
+		aura: AuraConfig {
+			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+		},
+		grandpa: GrandpaConfig {
+			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
+		},
+		sudo: SudoConfig {
+			// Assign network admin rights.
+			key: Some(root_key),
+		},
+		transaction_payment: Default::default(),
+	}
 }
 
 /// Configure initial storage state for FRAME modules.
---
+++ scripts/key/run.sh
@@ -0,0 +1,8 @@
+curl http://localhost:9933/ -H "Content-Type: application/json" -d @aura1
+curl http://localhost:9933/ -H "Content-Type: application/json" -d @gran1
+
+curl http://localhost:9934/ -H "Content-Type: application/json" -d @aura2
+curl http://localhost:9934/ -H "Content-Type: application/json" -d @gran2
+
+curl http://localhost:9935/ -H "Content-Type: application/json" -d @gran3
+curl http://localhost:9935/ -H "Content-Type: application/json" -d @gran3
